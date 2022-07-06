c7fe0c4939f522bdd1a7ed7ad9f33da64f2a76dd
Dmitri Kuznetsov <kna_int@mail.ru>
2022-07-06 22:15:10 +0300

    Added gameplay hud

--- scenes/ui/GameplayHUD.cs
+++ scenes/ui/GameplayHUD.cs
@@ -3,8 +3,20 @@ using System;
 
 public class GameplayHUD : CanvasLayer
 {
+    private Label _scoreLabel;
+
+    [Export]
+    public int Score;
+
     public override void _Ready()
     {
-        
+        _scoreLabel = GetNode<Label>("ScoreLabel");
+    }
+
+    public void OnScoreTimerTimeout()
+    {
+        Score++;
+
+        _scoreLabel.Text = Score.ToString();
     }
 }
