68c4c836045a775b34bb7214e69daffd4f4a7dba
Dmitri Kuznetsov <kna_int@mail.ru>
2022-07-07 23:17:15 +0300

    Added timeout

--- scenes/gameplay/Gameplay.cs
+++ scenes/gameplay/Gameplay.cs
@@ -5,4 +5,13 @@ public class Gameplay : Node
 {
     [Export]
     public PackedScene MobScene;
+
+    public int Score;
+
+    public void OnScoreTimerTimeout()
+    {
+        Score++;
+
+        GetNode<GameplayHUD>("GameplayHUD").UpdateScore(Score);
+    }
 }
