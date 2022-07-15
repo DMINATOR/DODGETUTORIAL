1e2ac716722825b923bd0426afb44a0e189eba67
Dmitri Kuznetsov <kna_int@mail.ru>
2022-07-15 23:30:39 +0300

    Fading almost works

--- autoLoad/GlobalAudioManager.cs
+++ autoLoad/GlobalAudioManager.cs
@@ -10,21 +10,77 @@ using System.Linq.Expressions;
 /// </summary>
 public class GlobalAudioManager : Node
 {
-    List<AudioStreamPlayer> _music;
+    // Currently playing music
+    AudioStreamPlayer _currentMusic;
+
+    // Music that is expired and has to fade out
+    List<AudioStreamPlayer> _expiredMusic = new List<AudioStreamPlayer>();
+
+    // Folders to store child nodes
+    Node _music;
+    Node _sounds;
+
+    // Timer to apply sounds effects
+    Timer _musicFadeInOutTimer;
+
+    private const float MinVolume = -80;
+    private const float MaxVolume = 0;
+    private const float IncreaseByStep = 10;
 
     public override void _Ready()
     {
-        GD.Print( $"Path is = {GetPath()}");
-
+        _music = GetNode<Node>("Music");
+        _sounds = GetNode<Node>("Sounds");
+        _musicFadeInOutTimer = GetNode<Timer>("MusicFadeInOutTimer");
     }
 
-    public void PlayMusic(AudioStream music)
+    public void PlayMusic(AudioStream audioStream)
     {
-        var newMusic = new AudioStreamPlayer();
-        newMusic.Stream = music;
-        newMusic.Play();
+        var newMusic = CreateAudioStreamPlayer(audioStream);
+        _music.AddChild(newMusic);
 
-        AddChild(newMusic);
+        if ( _currentMusic != null )
+        {
+            // Expire current music
+            _expiredMusic.Add(_currentMusic);
+        }
+
+        // Start playing
+        _currentMusic = newMusic;
+        _currentMusic.VolumeDb = MinVolume;
+        _currentMusic.Play();
+
+        CheckTimer();
+    }
+
+    private void CheckTimer()
+    {
+        //var shouldPause = _expiredMusic.Count == 0 && _currentMusic == null;
+
+        //GD.Print($"Timer {_musicFadeInOutTimer.Paused} -> {shouldPause}");
+
+        //_musicFadeInOutTimer.Paused = shouldPause;
+    }
+
+    private AudioStreamPlayer CreateAudioStreamPlayer(AudioStream audioStream)
+    {
+        return new AudioStreamPlayer
+        {
+            Stream = audioStream
+        };
+    }
+
+    public void MusicFadeInOutTimerTimeout()
+    {
+        // Increase current music volume
+        if( _currentMusic.VolumeDb < MaxVolume )
+        {
+            GD.Print($"Volume {_currentMusic.VolumeDb} -> +{IncreaseByStep}");
+
+            _currentMusic.VolumeDb += IncreaseByStep;
+        }
+
+        CheckTimer();
     }
 }
 
